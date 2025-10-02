ff75396279f070d1aa63e04c35250057cd4ba4d4
MAHossain1 <mahpro110@gmail.com>
2025-10-02 16:21:31 +0600

    provisioned ec2 with configured docker nginx

---
+++ entry-script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+yum update -y
+yum install -y docker
+systemctl enable docker --now
+docker run -d -p 8080:80 nginx
--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable avail_zone {}
 variable env_prefix {} 
 variable my_ip {}
 variable instance_type {}
+variable public_key_location {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -143,7 +144,13 @@ data "aws_ami" "latest-amazon-linux-image" {
 }
 
 output name {
-  value = data.aws_ami.latest-amazon-linux-image.id
+  value = aws_instance.myapp-server.public_ip
+}
+
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "ec2-provision-key"
+    public_key = file(var.public_key_location)
 }
 
 
@@ -157,7 +164,9 @@ resource "aws_instance" "myapp-server" {
     availability_zone = var.avail_zone
 
     associate_public_ip_address = true
-    key_name = "dockerServerKey"
+    key_name = aws_key_pair.ssh-key.key_name
+
+    user_data = file("entry-script.sh")
 
 
     tags = {
