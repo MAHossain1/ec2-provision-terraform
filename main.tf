5b29d8fc7654725286241e4378cc4a3dff97b6f9
MAHossain1 <mahpro110@gmail.com>
2025-10-03 08:19:47 +0600

    df

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
 provider "aws" {}
 
-variable vpc_cidr_block {} 
-variable subnet_cidr_block {}
-variable avail_zone {} 
-variable env_prefix {} 
-variable my_ip {}
-variable instance_type {}
-variable public_key_location {}
-variable private_key_location {}
-
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
@@ -16,89 +7,15 @@ resource "aws_vpc" "myapp-vpc" {
     }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.myapp-vpc.id 
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-        Name = "${var.env_prefix}-subnet-1"
-    }
-}
-
-
-/*
-resource "aws_route_table" "myapp-route-table" {
-    vpc_id = aws_vpc.myapp-vpc.id 
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-    tags = {
-        Name = "${var.env_prefix}-rtb"
-    }
-}
-*/
-
-resource "aws_internet_gateway" "myapp-igw" {
-    vpc_id = aws_vpc.myapp-vpc.id 
-    tags = {
-        Name = "${var.env_prefix}-igw"
-    }
-}
-
-/*
-resource "aws_route_table_association" "a-rtb-subnet" {
-    subnet_id = aws_subnet.myapp-subnet-1.id
-    route_table_id = aws_route_table.myapp-route-table.id
-}
-*/
-
-resource "aws_default_route_table" "main-rtb" {
-    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-    tags = {
-        Name = "${var.env_prefix}-main-rtb"
-    }
-}
-
-# custom security group
-/*    
-resource "aws_security_group" "myapp-sg" {
-    name = "myapp-sg"
+module "myapp-subnet" {
+    source = "./modules/subnet"
     vpc_id = aws_vpc.myapp-vpc.id
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.my_ip]
-    }
-
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        prefix_list_ids = []
-    }
-
-    tags = {
-        Name = "${var.env_prefix}-sg"
-    }
+    default_route_table_id =aws_vpc.myapp-vpc.default_route_table_id
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
 }
-*/
+
 
 resource "aws_default_security_group" "default-sg" {
     vpc_id = aws_vpc.myapp-vpc.id
@@ -168,33 +85,6 @@ resource "aws_instance" "myapp-server" {
     key_name = aws_key_pair.ssh-key.key_name
 
     user_data = file("entry-script.sh")
-
-    /* not recommended
-
-       # Connection block: defines how Terraform connects to the EC2 instance for provisioning.
-    connection {
-        type       = "ssh"  # Use SSH for connection
-        host       = self.public_ip  # Connect to the instance's public IP
-        user       = "ec2-user"  # Default user for Amazon Linux
-        private_key = file(var.private_key_location)  # Path to your private SSH key
-    }
-
-    # File provisioner: uploads a local script to the EC2 instance.
-    provisioner "file" {
-        source      = "entry-script.sh"  # Local script to upload
-        destination = "/home/ec2-user/entry-script-on-ec2.sh"  # Destination path on the instance
-    }
-
-    # Remote-exec provisioner: runs a script on the EC2 instance after upload.
-    provisioner "remote-exec" {
-        script = file("entry-script-on-ec2.sh")  # Script to execute on the instance
-    }
-
-    # Local-exec provisioner: runs a command locally on your machine after instance creation.
-    provisioner "local-exec" {
-        command = "echo ${self.public_ip} > output.txt"  # Save the instance's public IP to a
-
-    */
 
     tags = {
         Name = "${var.env_prefix}-server"
